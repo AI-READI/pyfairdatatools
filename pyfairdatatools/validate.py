3f233817ff23f5172ee96ea65d9c9c25a1e0c8c2
Sanjay Soundarajan <sanjay.16@live.com>
2023-05-15 18:09:02 -0700

    feat: add dataset_description validation schema

--- pyfairdatatools/validate.py
+++ pyfairdatatools/validate.py
@@ -1,3 +1,6 @@
+import json
+from os import path
+
 from jsonschema import validate
 
 schema = {
@@ -18,3 +21,28 @@ validate(instance={"name": "John", "age": 30, "scores": ["B", "A"]}, schema=sche
 
 validate(instance={"name": "John", "age": 30, "scores": []}, schema=schema)
 # No error, the JSON is valid.
+
+
+def validate_dataset_description(data):
+    """Validate a dataset description against the schema.
+
+    Args:
+        data (dict): The dataset description to validate
+    Returns:
+        bool: True if the dataset description is valid, False otherwise
+    """
+    schema = {}
+
+    # Import the schema from the schemas folder
+    with open(
+        path.join(path.dirname(__file__), "schemas", "dataset_description_schema.json"),
+        encoding="utf-8",
+    ) as f:
+        schema = json.load(f)
+
+    try:
+        validate(instance=data, schema=schema)
+        return True
+    except Exception as e:
+        print(e)
+        return False
