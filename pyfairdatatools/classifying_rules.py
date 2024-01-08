7e8e632a0261f464eb645d1076f76dc4a94c76df
slugb0t <wheresdorian@gmail.com>
2024-01-08 10:27:03 -0800

    :recycle: refactor: Add none case instead of N/A string for mypy error

--- pyfairdatatools/classifying_rules.py
+++ pyfairdatatools/classifying_rules.py
@@ -345,19 +345,8 @@ def extract_dicom_entry(file):
     # unknown
     else:
         sopinstanceuid = f"Unknown SOP Class UID: {sopclassuid}"
-        laterality = (
-            device
-        ) = (
-            rows
-        ) = (
-            referencedsopinstance
-        ) = (
-            implementationversion
-        ) = (
-            columns
-        ) = (
-            framenumber
-        ) = slicethickness = privatetag = gaze = numberoffiles = softwareversion = "N/A"
+        laterality = device = referencedsopinstance = implementationversion = "N/A"
+        rows = columns = framenumber = slicethickness = privatetag = gaze = numberoffiles = softwareversion = None
 
     output = DicomEntry(
         filename,
