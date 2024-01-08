c84f466472012c6d06c62868009ef87434da8a70
slugb0t <wheresdorian@gmail.com>
2024-01-08 11:39:33 -0800

    :bug: fix: formatting

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -687,7 +687,6 @@ class TestGenerateDatatypeDescription:
         file = tmp_path / "datatype_description.yaml"
         file_type = "yaml"
 
-
         generate_datatype_file(data=data, file_path=file, file_type=file_type)
 
         assert path.exists(file) is True
