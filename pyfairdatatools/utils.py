f4fe996f30e822f5f17a36ac3ac642b5dcc6815c
slugb0t <wheresdorian@gmail.com>
2024-03-18 15:47:38 -0700

    :recycle: refactor: update convert function for updated schema

--- pyfairdatatools/utils.py
+++ pyfairdatatools/utils.py
@@ -76,24 +76,26 @@ def convert_for_datacite(data):
     rights_list = []
     descriptions = []
 
-    for description in data["Description"]:
+    for description in data["description"]:
         description_obj = {
             "description": description["descriptionValue"],
             "descriptionType": description["descriptionType"],
         }
         descriptions.append(description_obj)
 
-    for rights in data["Rights"]:
-        rights_obj = {"rights": rights["rightsValue"]}
+    for rights in data["rights"]:
+        rights_obj = {"rights": rights["rightsName"]}
         if "rightsURI" in rights:
             rights_obj["rightsUri"] = rights["rightsURI"]
-        if "rightsIdentifier" in rights:
-            rights_obj["rightsIdentifier"] = rights["rightsIdentifier"]
-        if "rightsIdentifierScheme" in rights:
-            rights_obj["rightsIdentifierScheme"] = rights["rightsIdentifierScheme"]
+        if "rightsIdentifier" in rights and "rightsIdentifierValue" in rights["rightsIdentifier"]:
+            rights_obj["rightsIdentifier"] = rights["rightsIdentifier"]["rightsIdentifierValue"]
+        if "rightsIdentifier" in rights and "rightsIdentifierScheme" in rights["rightsIdentifier"]:
+            rights_obj["rightsIdentifierScheme"] = rights["rightsIdentifier"]["rightsIdentifierScheme"]
+        if "rightsIdentifier" in rights and "schemeURI" in rights["rightsIdentifier"]:
+            rights_obj["schemeUri"] = rights["rightsIdentifier"]["schemeURI"]
         rights_list.append(rights_obj)
 
-    for funder in data["FundingReference"]:
+    for funder in data["fundingReference"]:
         funder_obj = {
             "funderName": funder["funderName"],
             "funderIdentifier": funder["funderIdentifier"]["funderIdentifierValue"],
@@ -109,86 +111,86 @@ def convert_for_datacite(data):
             ]
         funding_references.append(funder_obj)
 
-    for related_item in data["RelatedItem"]:
-        if "relatedItemIdentifier" in related_item:
-            related_item_identifiers = []
-            for identifier in related_item["relatedItemIdentifier"]:
-                identifier_obj = {
-                    "relatedItemIdentifier": identifier["relatedItemIdentifierValue"],
-                    "relatedItemIdentifierType": identifier[
-                        "relatedItemIdentifierType"
-                    ],
-                }
-                if "relatedMetadataScheme" in identifier:
-                    identifier_obj["relatedMetadataScheme"] = identifier[
-                        "relatedMetadataScheme"
-                    ]
-                if "schemeURI" in identifier:
-                    identifier_obj["schemeUri"] = identifier["schemeURI"]
-                if "schemeType" in identifier:
-                    identifier_obj["schemeType"] = identifier["schemeType"]
+    # for related_item in data["RelatedItem"]:
+    #     if "relatedItemIdentifier" in related_item:
+    #         related_item_identifiers = []
+    #         for identifier in related_item["relatedItemIdentifier"]:
+    #             identifier_obj = {
+    #                 "relatedItemIdentifier": identifier["relatedItemIdentifierValue"],
+    #                 "relatedItemIdentifierType": identifier[
+    #                     "relatedItemIdentifierType"
+    #                 ],
+    #             }
+    #             if "relatedMetadataScheme" in identifier:
+    #                 identifier_obj["relatedMetadataScheme"] = identifier[
+    #                     "relatedMetadataScheme"
+    #                 ]
+    #             if "schemeURI" in identifier:
+    #                 identifier_obj["schemeUri"] = identifier["schemeURI"]
+    #             if "schemeType" in identifier:
+    #                 identifier_obj["schemeType"] = identifier["schemeType"]
 
-                related_item_identifiers.append(identifier_obj)
-        if "title" in related_item:
-            related_item_titles = []
-            for title in related_item["title"]:
-                title_obj = {"title": title["titleValue"]}
-                if "titleType" in title:
-                    title_obj["titleType"] = title["titleType"]
-                related_item_titles.append(title_obj)
-        if "creator" in related_item:
-            related_item_creators = []
-            for creator in related_item["creator"]:
-                creator_obj = {
-                    "name": creator["creatorName"],
-                    "nameType": creator["nameType"],
-                }
-                related_item_creators.append(creator_obj)
-        if "contributor" in related_item:
-            related_item_contributors = []
-            for contributor in related_item["contributor"]:
-                contributor_obj = {
-                    "name": contributor["contributorName"],
-                    "contributorType": contributor["contributorType"],
-                }
-                if "nameType" in contributor:
-                    contributor_obj["nameType"] = contributor["nameType"]
-                related_item_contributors.append(contributor_obj)
+    #             related_item_identifiers.append(identifier_obj)
+    #     if "title" in related_item:
+    #         related_item_titles = []
+    #         for title in related_item["title"]:
+    #             title_obj = {"title": title["titleValue"]}
+    #             if "titleType" in title:
+    #                 title_obj["titleType"] = title["titleType"]
+    #             related_item_titles.append(title_obj)
+    #     if "creator" in related_item:
+    #         related_item_creators = []
+    #         for creator in related_item["creator"]:
+    #             creator_obj = {
+    #                 "name": creator["creatorName"],
+    #                 "nameType": creator["nameType"],
+    #             }
+    #             related_item_creators.append(creator_obj)
+    #     if "contributor" in related_item:
+    #         related_item_contributors = []
+    #         for contributor in related_item["contributor"]:
+    #             contributor_obj = {
+    #                 "name": contributor["contributorName"],
+    #                 "contributorType": contributor["contributorType"],
+    #             }
+    #             if "nameType" in contributor:
+    #                 contributor_obj["nameType"] = contributor["nameType"]
+    #             related_item_contributors.append(contributor_obj)
 
-        related_item_obj = {
-            "relationType": related_item["relationType"],
-            "relatedItemType": related_item["relatedItemType"],
-        }
-        if related_item_creators:
-            related_item_obj["creators"] = related_item_creators
-        if related_item_contributors:
-            related_item_obj["contributors"] = related_item_contributors
-        if related_item_titles:
-            related_item_obj["titles"] = related_item_titles
-        if related_item_identifiers:
-            related_item_obj["relatedItemIdentifier"] = related_item_identifiers
-        if "publicationYear" in related_item:
-            related_item_obj["publicationYear"] = related_item["publicationYear"]
-        if "volume" in related_item:
-            related_item_obj["volume"] = related_item["volume"]
-        if "issue" in related_item:
-            related_item_obj["issue"] = related_item["issue"]
-        if "number" in related_item and "numberValue" in related_item["number"]:
-            related_item_obj["number"] = related_item["number"]["numberValue"]
-        if "number" in related_item and "numberType" in related_item["number"]:
-            related_item_obj["numberType"] = related_item["number"]["numberType"]
-        if "firstPage" in related_item:
-            related_item_obj["firstPage"] = related_item["firstPage"]
-        if "lastPage" in related_item:
-            related_item_obj["last_page"] = related_item["lastPage"]
-        if "publisher" in related_item:
-            related_item_obj["publisher"] = related_item["publisher"]
-        if "edition" in related_item:
-            related_item_obj["edition"] = related_item["edition"]
+    #     related_item_obj = {
+    #         "relationType": related_item["relationType"],
+    #         "relatedItemType": related_item["relatedItemType"],
+    #     }
+    #     if related_item_creators:
+    #         related_item_obj["creators"] = related_item_creators
+    #     if related_item_contributors:
+    #         related_item_obj["contributors"] = related_item_contributors
+    #     if related_item_titles:
+    #         related_item_obj["titles"] = related_item_titles
+    #     if related_item_identifiers:
+    #         related_item_obj["relatedItemIdentifier"] = related_item_identifiers
+    #     if "publicationYear" in related_item:
+    #         related_item_obj["publicationYear"] = related_item["publicationYear"]
+    #     if "volume" in related_item:
+    #         related_item_obj["volume"] = related_item["volume"]
+    #     if "issue" in related_item:
+    #         related_item_obj["issue"] = related_item["issue"]
+    #     if "number" in related_item and "numberValue" in related_item["number"]:
+    #         related_item_obj["number"] = related_item["number"]["numberValue"]
+    #     if "number" in related_item and "numberType" in related_item["number"]:
+    #         related_item_obj["numberType"] = related_item["number"]["numberType"]
+    #     if "firstPage" in related_item:
+    #         related_item_obj["firstPage"] = related_item["firstPage"]
+    #     if "lastPage" in related_item:
+    #         related_item_obj["last_page"] = related_item["lastPage"]
+    #     if "publisher" in related_item:
+    #         related_item_obj["publisher"] = related_item["publisher"]
+    #     if "edition" in related_item:
+    #         related_item_obj["edition"] = related_item["edition"]
 
-        related_items.append(related_item_obj)
+    #     related_items.append(related_item_obj)
 
-    for alternate_identifier in data["AlternateIdentifier"]:
+    for alternate_identifier in data["alternateIdentifier"]:
         alternate_identifiers.append(
             {
                 "alternateIdentifier": alternate_identifier["alternateIdentifierValue"],
@@ -198,7 +200,7 @@ def convert_for_datacite(data):
             }
         )
 
-    for date in data["Date"]:
+    for date in data["date"]:
         date_obj = {
             "date": date["dateValue"],
             "dateType": date["dateType"],
@@ -207,23 +209,23 @@ def convert_for_datacite(data):
             date_obj["dateInformation"] = date["dateInformation"]
         dates.append(date_obj)
 
-    for contributor in data["Contributor"]:
+    for contributor in data["contributor"]:
         if "affiliation" in contributor:
             contributor_affiliations = []
             for affiliation in contributor["affiliation"]:
                 # TODO: VERIFY BY KEY IS AFFILIATIONVALUE AND NOT NAME
                 affiliate = {
-                    "name": affiliation["affiliationValue"],
+                    "name": affiliation["affiliationName"],
                 }
-                if "schemeURI" in affiliation:
-                    affiliate["schemeUri"] = affiliation["schemeURI"]
-                if "affiliationIdentifierScheme" in affiliation:
-                    affiliate["affiliationIdentifierScheme"] = affiliation[
+                if "affiliationIdentifier" in affiliation and "schemeURI" in affiliation["affiliationIdentifier"]:
+                    affiliate["schemeUri"] = affiliation["affiliationIdentifier"]["schemeURI"]
+                if "affiliationIdentifier" in affiliation and "affiliationIdentifierScheme" in affiliation["affiliationIdentifier"]:
+                    affiliate["affiliationIdentifierScheme"] = affiliation["affiliationIdentifier"][
                         "affiliationIdentifierScheme"
                     ]
-                if "affiliationIdentifier" in affiliation:
-                    affiliate["affiliationIdentifier"] = affiliation[
-                        "affiliationIdentifier"
+                if "affiliationIdentifier" in affiliation and "affiliationIdentifierValue" in affiliation["affiliationIdentifier"]:
+                    affiliate["affiliationIdentifier"] = affiliation["affiliationIdentifier"][
+                        "affiliationIdentifierValue"
                     ]
 
                 contributor_affiliations.append(affiliate)
@@ -250,7 +252,7 @@ def convert_for_datacite(data):
 
         contributors.append(contributor_obj)
 
-    for subject in data["Subject"]:
+    for subject in data["subject"]:
         subject_obj = {}
         if "classificationCode" in subject:
             subject_obj["classificationCode"] = subject["classificationCode"]
@@ -261,28 +263,28 @@ def convert_for_datacite(data):
         subject_obj["subject"] = subject["subjectValue"]
         subjects.append(subject_obj)
 
-    for title in data["Title"]:
+    for title in data["title"]:
         title_obj = {"title": title["titleValue"]}
         if "titleType" in title:
             title_obj["titleType"] = title["titleType"]
         titles.append(title_obj)
 
-    for creator in data["Creator"]:
+    for creator in data["creator"]:
         if "affiliation" in creator:
             creator_affiliations = []
             for affiliation in creator["affiliation"]:
                 affiliate = {
-                    "name": affiliation["affiliationValue"],
+                    "name": affiliation["affiliationName"],
                 }
-                if "schemeURI" in affiliation:
-                    affiliate["schemeUri"] = affiliation["schemeURI"]
-                if "affiliationIdentifierScheme" in affiliation:
-                    affiliate["affiliationIdentifierScheme"] = affiliation[
+                if "affiliationIdentifier" in affiliation and "schemeURI" in affiliation:
+                    affiliate["schemeUri"] = affiliation["affiliationIdentifier"]["schemeURI"]
+                if "affiliationIdentifier" in affiliation and "affiliationIdentifierScheme" in affiliation:
+                    affiliate["affiliationIdentifierScheme"] = affiliation["affiliationIdentifier"][
                         "affiliationIdentifierScheme"
                     ]
-                if "affiliationIdentifier" in affiliation:
-                    affiliate["affiliationIdentifier"] = affiliation[
-                        "affiliationIdentifier"
+                if "affiliationIdentifier" in affiliation and "affiliationIdentifier" in affiliation:
+                    affiliate["affiliationIdentifier"] = affiliation["affiliationIdentifier"][
+                        "affiliationIdentifierValue"
                     ]
 
                 creator_affiliations.append(affiliate)
@@ -308,7 +310,7 @@ def convert_for_datacite(data):
 
         creators.append(creator_obj)
 
-    for funding_reference in data["FundingReference"]:
+    for funding_reference in data["fundingReference"]:
         funder_obj = {"funderName": funding_reference["funderName"]}
         if (
             "funderIdentifier" in funding_reference
@@ -333,27 +335,27 @@ def convert_for_datacite(data):
                 "doi": doi,
                 "creators": creators,
                 "titles": titles,
-                "publisher": {"name": data["Publisher"]},
-                "publicationYear": data["PublicationYear"],
+                "publisher": {"name": data["publisher"]},
+                "publicationYear": data["publicationYear"],
                 "subjects": subjects,
-                "contributors": data["Contributor"],
+                "contributors": contributors,
                 "dates": dates,
                 "alternateIdentifiers": alternate_identifiers,
-                "language": data["Language"],
-                "types": data["ResourceType"],
-                "relatedItems": related_items,
+                "language": data["language"],
+                "types": data["resourceType"],
+                # "relatedItems": related_items,
                 "rightsList": rights_list,
                 "description": descriptions,
-                "version": data["Version"],
+                "version": data["version"],
                 "fundingReferences": funding_references,
                 "url": "https://staging.fairhub.io/datasets/2",
             },
         }
     }
 
-    if len(data["RelatedIdentifier"]) > 0:
-        payload["data"]["attributes"]["relatedIdentifiers"] = data["RelatedIdentifier"]
-    if len(data["Size"]) > 0:
-        payload["data"]["attributes"]["sizes"] = data["Size"]
+    if len(data["relatedIdentifier"]) > 0:
+        payload["data"]["attributes"]["relatedIdentifiers"] = data["relatedIdentifier"]
+    if len(data["size"]) > 0:
+        payload["data"]["attributes"]["sizes"] = data["size"]
 
     return payload
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,8 +4,10 @@
 # import os
 
 import pytest
+from copy import deepcopy
+from typing import Any, Dict
 
-from pyfairdatatools.utils import feet_to_meters, requestJSON, validate_file_path
+from pyfairdatatools.utils import feet_to_meters, requestJSON, validate_file_path, convert_for_datacite
 
 
 class TestDescribeFeetToMeters:
@@ -41,3 +43,213 @@ class TestValidateFilePath:
     #         root_file_path = os.path.join(os.path.abspath(os.sep), "README.md")
     #         print(root_file_path)
     #         validate_file_path(root_file_path, writable=True)
+
+
+class TestConvertToDatacite:
+    """Test the convert_to_datacite function."""
+    valid_data: Dict[str, Any] = {
+        "identifier": {
+            "identifierValue": "10.5281/zenodo.1234567",
+            "identifierType": "DOI",
+        },
+        "title": [
+            {
+                "titleValue": "Main Title",
+            },
+            {
+                "titleValue": "Subtitle",
+                "titleType": "Subtitle",
+            },
+        ],
+        "version": "1.0.0",
+        "alternateIdentifier": [
+            {
+                "alternateIdentifierValue": "10.5281/zenodo.1234567",
+                "alternateIdentifierType": "DOI",
+            }
+        ],
+        "creator": [
+            {
+                "creatorName": "Doe, John",
+                "nameType": "Personal",
+                "nameIdentifier": [
+                    {
+                        "nameIdentifierValue": "0000-0001-2345-6789",
+                        "nameIdentifierScheme": "ORCID",
+                        "schemeURI": "https://orcid.org",
+                    }
+                ],
+                "affiliation": [
+                    {
+                        "affiliationName": "White Lotus",
+                        "affiliationIdentifier": {
+                            "affiliationIdentifierValue": "https://ror.org/123456789",
+                            "affiliationIdentifierScheme": "ROR",
+                            "schemeURI": "https://ror.org",
+                        },
+                    }
+                ],
+            },
+            {
+                "creatorName": "White Lotus Research",
+                "nameType": "Organizational",
+                "nameIdentifier": [
+                    {
+                        "nameIdentifierValue": "0000-0001-2345-6789",
+                        "nameIdentifierScheme": "ROR",
+                        "schemeURI": "https://ror.org",
+                    }
+                ],
+            },
+        ],
+        "contributor": [
+            {
+                "contributorType": "ContactPerson",
+                "contributorName": "Doe, John",
+                "nameType": "Personal",
+                "nameIdentifier": [
+                    {
+                        "nameIdentifierValue": "0000-0001-2345-6789",
+                        "nameIdentifierScheme": "ORCID",
+                        "schemeURI": "https://orcid.org",
+                    }
+                ],
+                "affiliation": [
+                    {
+                        "affiliationName": "White Lotus",
+                        "affiliationIdentifier": {
+                            "affiliationIdentifierValue": "https://ror.org/123456789",
+                            "affiliationIdentifierScheme": "ROR",
+                            "schemeURI": "https://ror.org",
+                        },
+                    }
+                ],
+            },
+            {
+                "contributorType": "HostingInstitution",
+                "contributorName": "White Lotus Research",
+                "nameType": "Organizational",
+                "nameIdentifier": [
+                    {
+                        "nameIdentifierValue": "0000-0001-2345-6789",
+                        "nameIdentifierScheme": "ROR",
+                        "schemeURI": "https://ror.org",
+                    }
+                ],
+            },
+        ],
+        "publicationYear": "2023",
+        "date": [
+            {
+                "dateValue": "2023-01-01",
+                "dateType": "Collected",
+                "dateInformation": "Some information",
+            }
+        ],
+        "resourceType": {
+            "resourceTypeValue": "Diabetes",
+            "resourceTypeGeneral": "Dataset",
+        },
+        "datasetDeIdentLevel": {
+            "deIdentType": "NoDeIdentification",
+            "deIdentDirect": True,
+            "deIdentHIPAA": True,
+            "deIdentDates": True,
+            "deIdentNonarr": True,
+            "deIdentKAnon": True,
+            "deIdentDetails": "Some details",
+        },
+        "datasetConsent": {
+            "consentType": "NoRestriction",
+            "consentNoncommercial": True,
+            "consentGeogRestrict": True,
+            "consentResearchType": True,
+            "consentGeneticOnly": True,
+            "consentNoMethods": True,
+            "consentsDetails": "Some details",
+        },
+        "description": [
+            {"descriptionValue": "Some description", "descriptionType": "Abstract"},
+            {"descriptionValue": "Some description", "descriptionType": "Methods"},
+        ],
+        "language": "en",
+        "relatedIdentifier": [
+            {
+                "relatedIdentifierValue": "10.5281/zenodo.1234567",
+                "relatedIdentifierType": "DOI",
+                "relationType": "IsCitedBy",
+                "relatedMetadataScheme": "DataCite",
+                "schemeURI": "https://schema.datacite.org/meta/kernel-4.3/doc/DataCite-MetadataKernel_v4.3.pdf",  # noqa: E501 pylint: disable=line-too-long
+                "schemeType": "DOI",
+                "resourceTypeGeneral": "Dataset",
+            }
+        ],
+        "subject": [
+            {
+                "subjectValue": "Diabetes",
+                "subjectIdentifier": {
+                    "classificationCode": "E11.9",
+                    "subjectScheme": "MeSH",
+                    "schemeURI": "https://www.nlm.nih.gov/mesh/",
+                    "valueURI": "https://www.nlm.nih.gov/mesh/1234567",
+                },
+            }
+        ],
+        "managingOrganization": {
+            "name": "Test Organization",
+            "managingOrganizationIdentifier": {
+                "managingOrganizationIdentifierValue": "04z8jg394",
+                "managingOrganizationScheme": "ROR",
+                "schemeURI": "https://www.crossref.org/",
+            },
+        },
+        "accessType": "PublicOnScreenAccess",
+        "accessDetails": {
+            "description": "Some description",
+            "url": "https://example.com",
+            "urlLastChecked": "2021-01-01",
+        },
+        "rights": [
+            {
+                "rightsName": "CC0-1.0",
+                "rightsURI": "https://creativecommons.org/publicdomain/zero/1.0/",
+                "rightsIdentifier": {
+                    "rightsIdentifierValue": "CC0-1.0",
+                    "rightsIdentifierScheme": "SPDX",
+                    "schemeURI": "https://spdx.org/licenses/",
+                },
+            }
+        ],
+        "publisher": {
+            "publisherName": "Test Publisher",
+            "publisherIdentifier": {
+                "publisherIdentifierValue": "04z8jg394",
+                "publisherIdentifierScheme": "ROR",
+                "schemeURI": "https://www.crossref.org/",
+            },
+        },
+        "size": ["15 pages", "15 MB"],
+        "format": ["application/pdf", "text/xml", "MOPG", "nifti"],
+        "fundingReference": [
+            {
+                "funderName": "Test Funder",
+                "funderIdentifier": {
+                    "funderIdentifierValue": "1234567",
+                    "funderIdentifierType": "Crossref Funder ID",
+                    "schemeURI": "https://doi.org/10.13039/501100001711",
+                },
+                "awardNumber": {
+                    "awardNumberValue": "1234567",
+                    "awardURI": "https://doi.org/10.13039/501100001711",
+                },
+                "awardTitle": "Test Award",
+            }
+        ],
+    }
+
+    def test_when_valid_data(self):
+        data = deepcopy(self.valid_data)
+        
+        output = convert_for_datacite(data)
+
+        assert output is not None
