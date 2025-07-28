8926b7953cccf262ba1f805a2d8f85ba84ce96c5
aydawka <aydan.gasimova2@gmail.com>
2025-07-28 10:32:10 -0700

    fix: pulling schema statement

--- dev/pull_schemas.py
+++ dev/pull_schemas.py
@@ -5,11 +5,11 @@ import requests
 
 schema_paths = [
     {
-        "sourceURL": "https://raw.githubusercontent.com/AI-READI/cds-specification/main/docs/public/schemas/dataset_description.schema.json",  # noqa: E501 # pylint: disable=line-too-long
+        "sourceURL": "https://raw.githubusercontent.com/AI-READI/cds-specification/main/docs/schemas/dataset_description.schema.json",  # noqa: E501 # pylint: disable=line-too-long
         "destinationFileName": "dataset_description.schema.json",
     },
     {
-        "sourceURL": "https://raw.githubusercontent.com/AI-READI/cds-specification/main/docs/public/schemas/study_description.schema.json",  # noqa: E501 # pylint: disable=line-too-long
+        "sourceURL": "https://raw.githubusercontent.com/AI-READI/cds-specification/main/docs/schemas/study_description.schema.json",  # noqa: E501 # pylint: disable=line-too-long
         "destinationFileName": "study_description.schema.json",
     },
     {
