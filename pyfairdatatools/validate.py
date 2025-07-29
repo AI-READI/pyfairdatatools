2a55a953bf719cb3c6051b0fd87a24ed065db0fc
aydawka <aydan.gasimova2@gmail.com>
2025-07-29 12:16:08 -0700

    fix: validation error handling

--- pyfairdatatools/validate.py
+++ pyfairdatatools/validate.py
@@ -113,7 +113,6 @@ def validate_study_description(data):  # sourcery skip: extract-method, low-code
         studyType = data["designModule"]["studyType"]
 
         if studyType == "Interventional":
-
             armGroupList = data["armsInterventionsModule"]["armGroupList"]
 
             for armGroup in armGroupList:
@@ -122,31 +121,8 @@ def validate_study_description(data):  # sourcery skip: extract-method, low-code
                         "armGroupType is required is required for interventional studies."  # pylint: disable=line-too-long
                     )
                     return False
-            if "targetDuration" in data["designModule"]:
-                del data["designModule"]["targetDuration"]
-
-            if "numberGroupsCohorts" in data["designModule"]:
-                del data["designModule"]["numberGroupsCohorts"]
-
-            if "bioSpec" in data["designModule"]:
-                del data["designModule"]["bioSpec"]
-
-            if "studyPopulation" in data["eligibilityModule"]:
-                del data["eligibilityModule"]["studyPopulation"]
-
-            if "samplingMethod" in data["eligibilityModule"]:
-                del data["eligibilityModule"]["SamplingMethod"]
 
         elif studyType == "Observational":
-            if "phaseList" in data["designModule"]:
-                del data["designModule"]["phaseList"]
-
-            if "numberArms" in data["designModule"]:
-                del data["designModule"]["numberArms"]
-
-            if "isPatientRegistry" in data["designModule"]:
-                del data["designModule"]["isPatientRegistry"]
-
             # check if the StudyPopulation key exists and is not empty
             if "studyPopulation" not in data["eligibilityModule"]:
                 print("studyPopulation is required for observational studies.")
@@ -164,7 +140,6 @@ def validate_study_description(data):  # sourcery skip: extract-method, low-code
             if "samplingMethod" not in data["eligibilityModule"]:
                 print("samplingMethod is required for observational studies.")
                 return False
-            
 
         if (
             "centralContactList" not in data["contactsLocationsModule"]
@@ -184,7 +159,9 @@ def validate_study_description(data):  # sourcery skip: extract-method, low-code
 
         return True
     except ValidationError as e:
-        print(e.schema["error_msg"] if "error_msg" in e.schema else e.message)
+        print(f" Validation Error: {e.message}")
+        print(f"→ Field Path: {'.'.join(str(p) for p in e.path)}")
+        print(f"→ Schema Rule Path: {'.'.join(str(p) for p in e.schema_path)}")
         return False
     except Exception as error:
         print(error)
