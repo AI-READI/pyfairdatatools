469f2e27b65a7a1c8e63259c64ea69b726c4186f
Aidan <62059163+Aydawka@users.noreply.github.com>
2025-08-04 21:20:22 -0700

    fix: test (#20)

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -18,6 +18,7 @@ from pyfairdatatools.generate import (
 class TestGenerateDatasetDescription:
     def test_valid_dataset_description(self, tmp_path):
         data = {
+            "schema": "https://schema.aireadi.org/v0.1.0/dataset_description.json",
             "identifier": {
                 "identifierValue": "10.5281/zenodo.1234567",
                 "identifierType": "DOI",
@@ -240,6 +241,7 @@ class TestGenerateDatasetDescription:
 class TestGenerateStudyDescription:
     def test_observational_study_description(self, tmp_path):
         data = {
+        "schema": "https://schema.aireadi.org/v0.1.0/study_description.json",
             "identificationModule": {
                 "officialTitle": "Test Title",
                 "acronym": "TT",
@@ -403,175 +405,176 @@ class TestGenerateStudyDescription:
 
         # TODO: add validation of the XML file
 
-    # def test_interventional_study_description(self, tmp_path):
-    #     data = {
-    #         "identificationModule": {
-    #             "officialTitle": "Test Title",
-    #             "acronym": "TT",
-    #             "orgStudyIdInfo": {
-    #                 "orgStudyId": "RandomStudyId",
-    #                 "orgStudyIdType": "Registry Identifier",
-    #                 "orgStudyIdDomain": "ClinicalTrials.gov",
-    #                 "orgStudyIdLink": "https://clinicaltrials.gov/ct2/show/NCT00000000",
-    #             },
-    #             "secondaryIdInfoList": [
-    #                 {
-    #                     "secondaryId": "SomeID",
-    #                     "secondaryIdType": "Other Identifier",
-    #                     "secondaryIdDomain": "Other",
-    #                     "secondaryIdLink": "https://example.com",
-    #                 }
-    #             ],
-    #         },
-    #         "statusModule": {
-    #             "overallStatus": "Suspended",
-    #             "whyStopped": "Study stopped due to lack of funding",
-    #             "startDateStruct": {
-    #                 "startDate": "2023-06",
-    #                 "startDateType": "Actual",
-    #             },
-    #             "completionDateStruct": {
-    #                 "completionDate": "2024-06",
-    #                 "completionDateType": "Actual",
-    #             },
-    #         },
-    #         "sponsorCollaboratorsModule": {
-    #             "responsibleParty": {
-    #                 "responsiblePartyType": "Principal Investigator",
-    #                 "responsiblePartyInvestigatorFirstName": "Harper",
-    #                 "responsiblePartyInvestigatorLastName": "Spiller",
-    #                 "responsiblePartyInvestigatorTitle": "Principal Investigator",
-    #                 "responsiblePartyInvestigatorAffiliation": {
-    #                     "responsiblePartyInvestigatorAffiliationName": "White Lotus",
-    #                 },
-    #             },
-    #             "leadSponsor": {"leadSponsorName": "Harper Spiller"},
-    #             "collaboratorList": [
-    #                 {"collaboratorName": "Nicole Mossbacher"},
-    #                 {"collaboratorName": "Olivia Mossbacher"},
-    #             ],
-    #         },
-    #         "oversightModule": {
-    #             "isFDARegulatedDrug": "No",
-    #             "isFDARegulatedDevice": "No",
-    #             "humanSubjectReviewStatus": "Request not yet submitted",
-    #             "oversightHasDMC": "No",
-    #         },
-    #         "descriptionModule": {
-    #             "briefSummary": "This is a brief summary",
-    #             "detailedDescription": "This is a detailed description",
-    #         },
-    #         "conditionsModule": {
-    #             "conditionList": [
-    #                 {"conditionName": "Condition 1"},
-    #                 {"conditionName": "Condition 2"},
-    #             ],
-    #             "keywordList": [
-    #                 {"keywordValue": "Keyword 1"},
-    #                 {"keywordValue": "Keyword 2"},
-    #             ],
-    #         },
-    #         "designModule": {
-    #             "studyType": "Interventional",
-    #             "designInfo": {
-    #                 "designAllocation": "Randomized",
-    #                 "designInterventionModel": "Prevention",
-    #                 "designInterventionModelDescription": "description",
-    #                 "designPrimaryPurpose": "Parallel Assignment",
-    #                 "designMaskingInfo": {
-    #                     "designMasking": "Blinded (no details)",
-    #                     "designMaskingDescription": "description of the design masking",
-    #                     "designWhoMaskedList": ["Participant", "Care Provider"],
-    #                 },
-    #             },
-    #             "phaseList": ["Phase 1/2"],
-    #             "enrollmentInfo": {
-    #                 "enrollmentCount": "34",
-    #                 "enrollmentType": "Anticipated",
-    #             },
-    #             "numberArms": "1",
-    #         },
-    #         "armsInterventionsModule": {
-    #             "armGroupList": [
-    #                 {
-    #                     "armGroupLabel": "Arm 1",
-    #                     "armGroupType": "Placebo Comparator",
-    #                     "armGroupDescription": "Experimental",
-    #                     "armGroupInterventionList": ["Drug 1"],
-    #                 }
-    #             ],
-    #             "interventionList": [
-    #                 {
-    #                     "interventionType": "Drug",
-    #                     "interventionName": "Drug 1",
-    #                     "interventionDescription": "description of the intervention",
-    #                     "interventionOtherNameList": ["Other Name 1"],
-    #                 },
-    #             ],
-    #         },
-    #         "eligibilityModule": {
-    #             "sex": "All",
-    #             "genderBased": "No",
-    #             "minimumAge": "18 Years",
-    #             "maximumAge": "65 Years",
-    #             "healthyVolunteers": "No",
-    #             "eligibilityCriteria": {
-    #                 "eligibilityCriteriaInclusion": ["crietia 1", "crietia 2"],
-    #                 "eligibilityCriteriaExclusion": ["crietia 1", "crietia 2"],
-    #             },
-    #         },
-    #         "contactsLocationsModule": {
-    #             "centralContactList": [
-    #                 {
-    #                     "centralContactFirstName": "Ethan",
-    #                     "centralContactLastName": "Spiller",
-    #                     "centralContactAffiliation": {
-    #                         "centralContactAffiliationName": "White Lotus",
-    #                     },
-    #                     "centralContactPhone": "805-555-5555",
-    #                     "centralContactPhoneExt": "123",
-    #                     "centralContactEMail": "e.spiller@hbo.com",
-    #                 }
-    #             ],
-    #             "overallOfficialList": [
-    #                 {
-    #                     "overallOfficialFirstName": "Daphne",
-    #                     "overallOfficialLastName": "Sullivan",
-    #                     "overallOfficialAffiliation": {
-    #                         "overallOfficialAffiliationName": "White Lotus",
-    #                     },
-    #                     "overallOfficialRole": "Study Principal Investigator",
-    #                 }
-    #             ],
-    #             "locationList": [
-    #                 {
-    #                     "locationFacility": "White Lotus",
-    #                     "locationStatus": "Recruiting",
-    #                     "locationCity": "Kihei",
-    #                     "locationState": "Hawaii",
-    #                     "locationZip": "96753",
-    #                     "locationCountry": "United States",
-    #                 }
-    #             ],
-    #         },
-    #     }
+    def test_interventional_study_description(self, tmp_path):
+        data = {
+            "schema": "https://schema.aireadi.org/v0.1.0/study_description.json",
+            "identificationModule": {
+                "officialTitle": "Test Title",
+                "acronym": "TT",
+                "orgStudyIdInfo": {
+                    "orgStudyId": "RandomStudyId",
+                    "orgStudyIdType": "Registry Identifier",
+                    "orgStudyIdDomain": "ClinicalTrials.gov",
+                    "orgStudyIdLink": "https://clinicaltrials.gov/ct2/show/NCT00000000",
+                },
+                "secondaryIdInfoList": [
+                    {
+                        "secondaryId": "SomeID",
+                        "secondaryIdType": "Other Identifier",
+                        "secondaryIdDomain": "Other",
+                        "secondaryIdLink": "https://example.com",
+                    }
+                ],
+            },
+            "statusModule": {
+                "overallStatus": "Suspended",
+                "whyStopped": "Study stopped due to lack of funding",
+                "startDateStruct": {
+                    "startDate": "2023-06",
+                    "startDateType": "Actual",
+                },
+                "completionDateStruct": {
+                    "completionDate": "2024-06",
+                    "completionDateType": "Actual",
+                },
+            },
+            "sponsorCollaboratorsModule": {
+                "responsibleParty": {
+                    "responsiblePartyType": "Principal Investigator",
+                    "responsiblePartyInvestigatorFirstName": "Harper",
+                    "responsiblePartyInvestigatorLastName": "Spiller",
+                    "responsiblePartyInvestigatorTitle": "Principal Investigator",
+                    "responsiblePartyInvestigatorAffiliation": {
+                        "responsiblePartyInvestigatorAffiliationName": "White Lotus",
+                    },
+                },
+                "leadSponsor": {"leadSponsorName": "Harper Spiller"},
+                "collaboratorList": [
+                    {"collaboratorName": "Nicole Mossbacher"},
+                    {"collaboratorName": "Olivia Mossbacher"},
+                ],
+            },
+            "oversightModule": {
+                "isFDARegulatedDrug": "No",
+                "isFDARegulatedDevice": "No",
+                "humanSubjectReviewStatus": "Request not yet submitted",
+                "oversightHasDMC": "No",
+            },
+            "descriptionModule": {
+                "briefSummary": "This is a brief summary",
+                "detailedDescription": "This is a detailed description",
+            },
+            "conditionsModule": {
+                "conditionList": [
+                    {"conditionName": "Condition 1"},
+                    {"conditionName": "Condition 2"},
+                ],
+                "keywordList": [
+                    {"keywordValue": "Keyword 1"},
+                    {"keywordValue": "Keyword 2"},
+                ],
+            },
+            "designModule": {
+                "studyType": "Interventional",
+                "designInfo": {
+                    "designAllocation": "Randomized",
+                    "designInterventionModel": "Prevention",
+                    "designInterventionModelDescription": "description",
+                    "designPrimaryPurpose": "Parallel Assignment",
+                    "designMaskingInfo": {
+                        "designMasking": "Blinded (no details)",
+                        "designMaskingDescription": "description of the design masking",
+                        "designWhoMaskedList": ["Participant", "Care Provider"],
+                    },
+                },
+                "phaseList": ["Phase 1/2"],
+                "enrollmentInfo": {
+                    "enrollmentCount": "34",
+                    "enrollmentType": "Anticipated",
+                },
+                "numberArms": "1",
+            },
+            "armsInterventionsModule": {
+                "armGroupList": [
+                    {
+                        "armGroupLabel": "Arm 1",
+                        "armGroupType": "Placebo Comparator",
+                        "armGroupDescription": "Experimental",
+                        "armGroupInterventionList": ["Drug 1"],
+                    }
+                ],
+                "interventionList": [
+                    {
+                        "interventionType": "Drug",
+                        "interventionName": "Drug 1",
+                        "interventionDescription": "description of the intervention",
+                        "interventionOtherNameList": ["Other Name 1"],
+                    },
+                ],
+            },
+            "eligibilityModule": {
+                "sex": "All",
+                "genderBased": "No",
+                "minimumAge": "18 Years",
+                "maximumAge": "65 Years",
+                "healthyVolunteers": "No",
+                "eligibilityCriteria": {
+                    "eligibilityCriteriaInclusion": ["crietia 1", "crietia 2"],
+                    "eligibilityCriteriaExclusion": ["crietia 1", "crietia 2"],
+                },
+            },
+            "contactsLocationsModule": {
+                "centralContactList": [
+                    {
+                        "centralContactFirstName": "Ethan",
+                        "centralContactLastName": "Spiller",
+                        "centralContactAffiliation": {
+                            "centralContactAffiliationName": "White Lotus",
+                        },
+                        "centralContactPhone": "805-555-5555",
+                        "centralContactPhoneExt": "123",
+                        "centralContactEMail": "e.spiller@hbo.com",
+                    }
+                ],
+                "overallOfficialList": [
+                    {
+                        "overallOfficialFirstName": "Daphne",
+                        "overallOfficialLastName": "Sullivan",
+                        "overallOfficialAffiliation": {
+                            "overallOfficialAffiliationName": "White Lotus",
+                        },
+                        "overallOfficialRole": "Study Principal Investigator",
+                    }
+                ],
+                "locationList": [
+                    {
+                        "locationFacility": "White Lotus",
+                        "locationStatus": "Recruiting",
+                        "locationCity": "Kihei",
+                        "locationState": "Hawaii",
+                        "locationZip": "96753",
+                        "locationCountry": "United States",
+                    }
+                ],
+            },
+        }
 
-    #     file = tmp_path / "study_description.json"
-    #     file_type = "json"
+        file = tmp_path / "study_description.json"
+        file_type = "json"
 
-    #     generate_study_description(data, file, file_type)
+        generate_study_description(data, file, file_type)
 
-    #     with open(file, "r", encoding="utf8") as f:
-    #         imported_data = json.load(f)
+        with open(file, "r", encoding="utf8") as f:
+            imported_data = json.load(f)
 
-    #     assert imported_data == data
+        assert imported_data == data
 
-    #     file = tmp_path / "study_description.xml"
-    #     file_type = "xml"
+        file = tmp_path / "study_description.xml"
+        file_type = "xml"
 
-    #     generate_study_description(data, file, file_type)
+        generate_study_description(data, file, file_type)
 
-    #     # TODO: add validation of the XML file
+        # TODO: add validation of the XML file
 
 
 class TestGenerateReadme:
